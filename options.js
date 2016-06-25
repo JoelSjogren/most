23bc5cc5c899e251e7b883454dd0bf8b7fc16a10
Joel Sjögren <joelsjogren.wii@gmail.com>
2016-06-25 12:30:38 +0200

    Better support for other languages

--- options.js
+++ options.js
@@ -8,11 +8,6 @@ function handle_input(e) {
 function repopulate_subtitles() {
     $(".most-subtitles").remove();
     
-    var samples = {
-        en: "<i>He doesn't have any friends in Korea so he probably doesn't even have anyone to eat with.</i>",
-        de: "<i> Er hat keine Freunde in Korea, also wird er wahrscheinlich niemanden haben, mit dem er was essen kann.</i>"
-    };  // TODO: add more sample languages
-    
     var languages = get_used_languages();
     for (var i = 0; i < languages.length; i++) {
         var div = document.createElement("div");
@@ -68,3 +63,39 @@ $("#config-area").on('input', handle_input);
 document.addEventListener('DOMContentLoaded', restore_options);
 $("#save")[0].addEventListener('click', save_options);
 $("#remove")[0].addEventListener('click', remove_storage);
+
+
+
+
+
+
+var samples = {
+    ar: "ليس لديه أي أصدقاء في كوريا , لذلك على الأرجح لا يملك من يأكل معه",
+    bg: "<i>Няма приятели в Корея, така че вероятно няма с кого да яде.</i>",
+    cs: "<i>V Koreji nikoho nezná takže ani nemá s kým jíst.</i>",
+    de: "<i> Er hat keine Freunde in Korea, also wird er wahrscheinlich niemanden haben, mit dem er was essen kann.</i>",
+    el: "<i>Δεν έχει φίλους στην Κορέα, οπότε <br>πιθανόν να μην έχει παρέα για να φάει.</i>",
+    en: "<i>He doesn't have any friends in Korea so he probably doesn't even have anyone to eat with.</i>",
+    es: "<i>Él no tiene ningún amigo en Corea, así que,<br> probablemente no tiene a nadie con quien comer.</i>",
+    et: "<i> Tal ei ole Koreas sõpru niiet tal ei ole arvatavasti isegi kellegagi koos süüa. </i>",
+    fi: "<i>Hänellä ei edes ole ystäviä Koreassa, joten hänellä ei varmasti ole ystäviä, joiden kanssa käydä syömässä.</i>",
+    fr: "<i>Il n'a aucun ami en Corée, alors il n'a probablement <br>personne avec qui manger.</i>",
+    he: ". אין לו חברים בכל בקוריאה ולכן הוא כנראה אפילו לא צריך אף אחד לאכול איתו",
+    hr: "<i> On nema prijatelja u Koreji, <br>tako da vjerojatno nema nikoga s kim bi jeo.</i>",
+    hu: "<i>Nincs sok barátja Koreában, így ennie sincs kivel.</i>",
+    id: "<i>Dia tidak punya teman satu pun di Korea, jadi dia mungkin tidak punya siapapun untuk makan dengannya.</i>",
+    it: "<i>Non ha amici in Corea e probabilmente<br>non ha neanche qualcuno con cui mangiare.</i>",
+    ja: "<i>韓国に友達がいないから<br>きっと一緒に食事する友達もいないんだわ</i>",
+    lt: "<i>Jis Korėjoje neturi jokių draugų, todėl, ko gero, neturi su kuo pavalgyti.</i>",
+    nl: "<i>Hij heeft geen vriendin in Korea, dus hij heeft waarschijnlijk niemand om mee samen te eten.</i>",
+    pl: "<i>Nie ma żadnych przyjaciół w Korei, więc pewnie nie ma z kim zjeść.</i>",
+    pt: "<i>Ele não tem nenhum amigo na Coreia, então provavelmente não tem ninguém com quem comer junto.</i>",
+    ro: "<i>Nu are nici un prieten in Coreea, asa ca<br> probabil nu are cu cine sa manance.</i>",
+    sh: "<i>Nema nikakve prijatelje u Koreji<br>sa kojima bi mogao da jede.</i>",
+    sk: "<i>Nemá v Kórei veľa priateľov, takže<br> nemá ani s kým jesť.</i>",
+    tl: "<i>May iba kaya siyang kaibigan sa Korea na kasabay niyang kumain.</i>",
+    tr: "<i>Muhtemelen yemek için kimsesi yok bu yüzden Kore'de çok fazla arkadaşı bile yok.</i>",
+    vi: "<i>Cậu ta không có người bạn nào ở Hàn vì thế cậu ấy có khả năng không có ai ăn cùng cả.</i>",
+    zh: "在韩国没有朋友 应该没有能一起吃饭的人吧",
+    zt: "在韓國沒有朋友 應該沒有能一起吃飯的人吧",
+};
