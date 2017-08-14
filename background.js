539074e68ff3982a79edf1d16e2fe45a04ef8e11
Joel Sjögren <joelsjogren.wii@gmail.com>
2017-08-14 21:51:17 +0200

    begin rewrite without downsub

--- background.js
+++ background.js
@@ -1,65 +1,14 @@
 // When the icon next to the address bar is clicked, subtitles start playing.
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.getSelected(null, function(tab) {
-        // Extract the video id based on the current url.
-        var known = ["viki.com/videos/", "subber.viki.com/translations/",
-            "subber.viki.com/subtitlers/"];
-        var id;
-        for (var i = 0; i < known.length; i++) {
-            if (tab.url.indexOf(known[i]) != -1) {
-                id = tab.url.split(known[i])[1];
-            }
-        }
-        id = id.split("?")[0];
-        
-        // Download all subtitles mentioned in the user-defined css.
-        var subtitles = {};
         with_style_and_inferred_languages(function(style, languages) {
-            wait = countdown(languages.length);
-            for (var i = 0; i < languages.length; i++) {
-                request(subtitles, id, languages[i], wait, function() {
-                    // Finally, display the subtitles.
-                    chrome.tabs.sendMessage(tab.id, {
-                        style: style,
-                        languages: languages,
-                        subtitles: subtitles
-                    });
-                });
-            }
+            chrome.tabs.sendMessage(tab.id, {
+                style: style,
+                languages: languages
+            });
         });
     });
 });
-
-// Make a request to DownSub and store the result in 'subtitles'.
-// Then either wait for other requests to finish or call the callback.
-function request(subtitles, videoId, language, wait, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(response) {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
-            if (xhr.status == 200) {
-                console.log("Obtained subtitles for " + language);
-                console.log(xhr.responseText);
-                subtitles[language] = xhr.responseText;
-            } else {
-                console.log("Failed to obtain subtitles for " + language);
-            }
-            if (!wait()) {
-                callback();
-            } else {
-                console.log("Waiting...");
-            }
-        }
-    };
-    var subtitleURL = 'http://downsub.com/index.php?title=' + videoId +
-        "&url=http%3A%2F%2Fviki.com%2F" + language;
-    xhr.open("GET", subtitleURL, true);
-    xhr.send();
-}
-
-// A simple counter: n-1, n-2, n-3, ...
-function countdown(n) {
-    return function () { return n -= 1; };
-}
 
 // Load options defined by user: css rules and subtitle languages.
 function with_style_and_inferred_languages(callback) {
--- overlay.js
+++ overlay.js
@@ -23,183 +23,101 @@ subtitles for the current episode! This means that DownSub could be replaced.
 */
 
 // One subtitle writer for each language.
-var writers = [];
 
 $(document).ready(function () {
     
     function init(message, sender, callback) {
-        // Stop the old "instance" if there is one.
-        clean();
-    
-        // Initiate communication.
-        startSync();
-        addOverlay(message.style);
-        writers = [];
-        for (var i = 0; i < message.languages.length; i++) {
-            var srt = message.subtitles[message.languages[i]];
-            writers[i] = new Writer(message.languages[i], srt);
-            writers[i].play();
-        }
+        alert("Alive!");
+        addOverlay();
+        setStyle(message.style);
+        addCueListeners(message.languages);
     }
 
     chrome.extension.onMessage.addListener(init);
 
 });
 
-function clean() {
-    for (var i = 0; i < writers.length; i++) {
-        writers[i].stop();
-    }
-    
-    $("#most-overlay").remove();
+function addOverlay(style) {
+    var overlay = $("<div id='most-overlay'></div>");
+    $("body").append(overlay);
 }
 
-function Writer(language, srt) {
-    // The decoded srt file is organized into pages.
-    this.pages = parseSubtitles(srt);
-    this.current;
-    
-    // Text is continuously written as html.
-    this.element = document.createElement("div");
-    this.element.className = "most-subtitles most-" + language;
-    $("#most-overlay")[0].appendChild(this.element);
-    
-    this.timer;
-
-    this.play = function () {
-        this.timer = true;
-        
-        var that = this;
-        
-        // To be run many times per second.
-        function updateRepeatedly() {
-            var period = 50;  // sleep time between updates
-            
-            // Get the local playback time (in milliseconds, starting at 0).
-            var syncEl = document.getElementById("most-sync");
-            var local = new Date().getTime() - parseInt(syncEl.innerHTML);
-            
-            // Write current subtitle page to screen.
-            for (var i = 0; i < that.pages.length; i++) {  // TODO binary search?
-                var page = that.pages[i];
-                // the " + period" here is just for fine tuning, not optimal
-                if (page['loc0'] <= local + period
-                        && local + period <= page['loc1']
-                        && that.current != language + "-" + i) {
-                    that.show(page['s'], page['loc1']-local, language + "-" + i);
-                }
-            }
-            
-            // Reschedule, creating an infinite loop.
-            if (that.timer) {
-                that.timer = window.setTimeout(updateRepeatedly, period);
-            }
-        };
-        
-        updateRepeatedly();
-    }
-    
-    this.stop = function() {
-        this.timer = false;
-    }
-
-    // Show text, typically a subtitle page.
-    this.show = function(text, duration, uid) {
-        // Remove old text. (This is an exceptional case.)
-        if (this.current !== undefined) {
-            this.hide(this.current);
-        }
-        
-        // Update video geometry. TODO: Should this line be somewhere else?
-        updateBasicCSS();
-    
-        // Write the text to screen.
-        var textEl = document.createElement("div");
-        textEl.className = "uid-" + uid;
-        textEl.innerHTML = text;
-        this.element.appendChild(textEl);
-        
-        // Don't rewrite it a lot.
-        this.current = uid;
-        
-        // Remember to remove it after a while.
-        var that = this;
-        window.setTimeout(function () {  // TODO: don't remove it if paused.
-            if (that.current === uid) {
-                that.hide(uid);
-                that.current = undefined;
-            }
-        }, duration);
-    }
-
-    // Hide text. Inverse of 'show'.
-    this.hide = function(uid) {
-        $(".uid-" + uid).remove();
-    }
-    
-    // Info text after trying to initialize the writer.
-    if (this.pages.length > 0) {
-        this.show("Loaded " + this.pages.length + " pages.", 2000,
-            language + "-info");
-    } else {
-        this.show("Failed to load subtitles for language " + language, 4000,
-            language + "-info");
-    }
-
+function setStyle(style) {
+    updateVideoRectangle();
+    var styleEl = document.createElement("style");
+    styleEl.innerHTML = style;
+    $("head").append(styleEl);
 }
 
-// Ugly hack independent of this scope (only its string will be injected).
-function specialCode () {
-    // Will hold the unix time minus local playback time (in milliseconds).
-    $("body").append("<div id='most-sync'></div>");
-    
-    // Determine how the target player will tell its current position
-    // (in seconds).
-    var getCurrentTime = function() {
-        // Look for the regular player, subtitle editor (flash or html5)
-        // and segment editor.
-        if ("player" in window) {
-            return player.getCurrentTime();
-        }
-        if ("html5player" in window) {
-            return html5player.player.getCurrentTime();
-        }
-        if ("subber_player_flash_api" in window) {
-            return subber_player_flash_api.player.currentTime();
-        }
-        if ("subber_player_html5_api" in window) {
-            return subber_player_html5_api.player.currentTime();
-        }
-    }
-    
-    // Export this position continuously in html format (using the DOM).
-    function syncForever() {
-        var local = Math.round(getCurrentTime() * 1000);
-        var syncVal = new Date().getTime() - local;
-        document.getElementById("most-sync").innerHTML = syncVal;
-        window.setTimeout(syncForever, 100);
-    };
-  
-    syncForever();
-}
-
-function startSync() {
-    // Inject the ugly hack into the web page.
+function addCueListeners(languages) {
+    // Inject listeners ('specialCode') into the webpage.
     var script = document.createElement('script');
     script.appendChild(document.createTextNode(
-        '('+ specialCode +')();'));
+        '('+ specialCode +')(' + JSON.stringify(languages) + ');'));
     (document.body || document.head || document.documentElement)
         .appendChild(script);
 }
 
-function updateBasicCSS() {
-    // Find the positioning of the video player, or use (bad) defaults.
+// To be injected -- as a string!
+function specialCode (languages) {
+    var player = html5player.player;
+    var tracks = player.subtitle.videojs.tech_.textTracks_;
+    
+    // Fill Viki's subtitle cache.
+    var old = player.getSubtitleLanguage();
+    for (var i = 0; i < languages.length; i++) {
+        player.setSubtitleLanguage(languages[i]);
+    }
+    player.setSubtitleLanguage(old);
+    
+    // Register listeners with that cache.
+    for (var i = 0; i < languages.length; i++) {
+        // Html entry for subtitles in one language.
+        var element = document.createElement("div");
+        element.className = "most-subtitles most-" + language;
+        $("#most-overlay")[0].appendChild(element);
+    
+        // Cue listener.
+        var j;  // Find a track with the right language.
+        for (j = 0; j < tracks.length; j++) {
+            if (tracks[j].language == languages[i]) break;
+        }
+        addCueListener(element, languages[i], tracks[j]);
+        
+        function addCueListener(element, language, track) {
+            track.addEventListener('cuechange', function() {
+                console.log("> cue change (" + language + ") <");
+                
+                var cue = "";
+                if (track.activeCues[0]) {
+                    cue = track.activeCues[0].text;
+                }
+                element.innerHTML = cue;
+                
+                // use this.element here
+                /*
+                // Remove old text. (This is an exceptional case.)
+                if (this.current !== undefined) {
+                    this.hide(this.current);
+                }
+            
+                // Write the text to screen.
+                var textEl = document.createElement("div");
+                textEl.className = "uid-" + uid;
+                textEl.innerHTML = text;
+                this.element.appendChild(textEl);
+                */
+            });
+        }
+    }
+}
+
+function updateVideoRectangle() {
+    // The video player.
     var video = $("#video-player_Shaka_api")[0] ||
         $("#video-player_html5_api")[0] || $("#subber_player")[0];
     var videoRect = video.getBoundingClientRect();
     
-    // Configure the looks and position.
-    //$("#most-overlay").removeAttr("style"); ?
     $("#most-overlay").css({
         position: "fixed",
         width: videoRect.width + "px",
@@ -212,61 +130,7 @@ function updateBasicCSS() {
     });
 }
 
-function addOverlay(style) {
-    // Create the overlay
-    overlay = $("<div id='most-overlay'></div>");
-    $("body").append(overlay);
-    
-    // Set the subtitle style
-    updateBasicCSS();
-    var styleEl = document.createElement("style");
-    styleEl.innerHTML = style;
-    $("head").append(styleEl);
-}
 
-// Convert an srt time string to integer.
-function strToMs(loc) {
-    var h, m, s, ms;
-    [h, m, s, ms] = loc.replace(',', ':').split(':');
-    [h, m, s, ms] = [parseInt(h), parseInt(m), parseInt(s), parseInt(ms)];
-    return ((h*60 + m)*60 + s)*1000 + ms;
-}
-
-// Turn an srt into computation-friendly form: pages.
-function parseSubtitles(srt) {
-    //console.log("parseSubtitles", srt);
-    
-    if (srt === undefined || srt === "") return [];
-
-    var pages = [];
-
-    var srt2 = srt.replace(/\r\n|\r|\n/g, '\n')  // Normalize newlines.
-                  .trim(srt)
-                  .split('\n\n');
-
-    // Parse each page.
-    for (var i = 0; i < srt2.length; i++) {
-        var a, b, c, d, e, loc0, loc1;
-        a = srt2[i];
-        [b, c] = [a.slice(0, a.indexOf("\n")), a.slice(a.indexOf("\n")+1)];
-        [d, e] = [c.slice(0, c.indexOf("\n")), c.slice(c.indexOf("\n")+1)];
-        [loc0, loc1] = d.split(" --> ");
-        
-        // The following line improves security but comes at the cost of
-        // not supporting arbitrary html markup.
-        e = e.replace(/<\/?[^>]+(>|$)/g, function(part) {
-            var accept = ['<i>', '</i>', '<br>', '</br>', '<b>', '</b>'];
-            for (var i = 0; i < accept.length; i++) {
-                if (part === accept[i]) return part;
-            }
-            return "";
-        });
-        
-        pages.push({'loc0': strToMs(loc0), 'loc1': strToMs(loc1), 's': e});
-    }
-    
-    return pages;
-}
 
 
 
